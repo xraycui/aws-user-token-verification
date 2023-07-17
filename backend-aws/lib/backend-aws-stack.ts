c444a78b9728b3d925e448baf114135ef9df036f
Xuesong Cui <xuesongcui@Xuesongs-MacBook-Air.local>
2023-07-17 13:54:11 -0400

    provision a step function workflow

--- backend-aws/lib/backend-aws-stack.ts
+++ backend-aws/lib/backend-aws-stack.ts
@@ -2,6 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import { ApiGateway } from './ApiGateway'
 import { Lambda } from './Lambda'
 import { Construct } from 'constructs';
+import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
+import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class BackendAwsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -20,6 +23,18 @@ export class BackendAwsStack extends cdk.Stack {
     api.addIntegration('DELETE', '/delete', deleteUserLambda)
 
     // step function
+    const state1 = new stepfunctions.Pass(this, 'State1');
+    const state2 = new stepfunctions.Pass(this, 'State2');
 
+    const definition = state1.next(state2);
+
+    const role = new iam.Role(this, 'VerifyTokenStateMachineRole', {
+      assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
+    });
+
+    const verifyTokenStateMachine = new stepfunctions.StateMachine(this, 'VerifyTokenStateMachine', {
+      definition: definition,
+      role: role,
+    });
   }
 }
