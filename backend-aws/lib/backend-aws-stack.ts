58adbb65d5ea8ccc5851fac6ed8f4cc475cc3b77
Xuesong Cui <xuesongcui@Xuesongs-MacBook-Air.local>
2023-07-17 15:19:21 -0400

    create verifyUserTokenStateMachine with defination json

--- backend-aws/lib/backend-aws-stack.ts
+++ backend-aws/lib/backend-aws-stack.ts
@@ -3,7 +3,6 @@ import { ApiGateway } from './ApiGateway'
 import { Lambda } from './Lambda'
 import { Construct } from 'constructs';
 import * as stepfunctions from 'aws-cdk-lib/aws-stepfunctions';
-import * as tasks from 'aws-cdk-lib/aws-stepfunctions-tasks';
 import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class BackendAwsStack extends cdk.Stack {
@@ -23,17 +22,15 @@ export class BackendAwsStack extends cdk.Stack {
     api.addIntegration('DELETE', '/delete', deleteUserLambda)
 
     // step function
-    const state1 = new stepfunctions.Pass(this, 'State1');
-    const state2 = new stepfunctions.Pass(this, 'State2');
-
-    const definition = state1.next(state2);
+    const verifyUserTokenLambda = new Lambda(this, 'verifyUserToken')
+    const definition = require('../src/step-function/definition')
 
     const role = new iam.Role(this, 'VerifyTokenStateMachineRole', {
       assumedBy: new iam.ServicePrincipal('states.amazonaws.com'),
     });
 
     const verifyTokenStateMachine = new stepfunctions.StateMachine(this, 'VerifyTokenStateMachine', {
-      definition: definition,
+      definitionBody: stepfunctions.DefinitionBody.fromChainable(definition),
       role: role,
     });
   }
---
+++ backend-aws/src/step-function/definition.ts
@@ -0,0 +1,74 @@
+export const definitionJson = {
+  "StartAt": "retrieveUser",
+  "States": {
+    "retrieveUser": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "OutputPath": "$.Payload",
+      "Parameters": {
+        "Payload.$": "$",
+        "FunctionName": "arn:aws:lambda:us-east-1:446650953357:function:BackendAwsStack-getUserB196FDAE-fh5DmGBpLJXf:$LATEST"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Next": "Found"
+    },
+    "Found": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Next": "verifyUserToken"
+        }
+      ]
+    },
+    "verifyUserToken": {
+      "Type": "Task",
+      "Resource": "arn:aws:states:::lambda:invoke",
+      "OutputPath": "$.Payload",
+      "Parameters": {
+        "Payload.$": "$",
+        "FunctionName": "arn:aws:lambda:us-east-1:446650953357:function:BackendAwsStack-verifyUserTokenE8BE21CF-4vQt4R9nc9DQ:$LATEST"
+      },
+      "Retry": [
+        {
+          "ErrorEquals": [
+            "Lambda.ServiceException",
+            "Lambda.AWSLambdaException",
+            "Lambda.SdkClientException",
+            "Lambda.TooManyRequestsException"
+          ],
+          "IntervalSeconds": 2,
+          "MaxAttempts": 6,
+          "BackoffRate": 2
+        }
+      ],
+      "Next": "Verified"
+    },
+    "Verified": {
+      "Type": "Choice",
+      "Choices": [
+        {
+          "Next": "Success"
+        }
+      ],
+      "Default": "Fail"
+    },
+    "Success": {
+      "Type": "Succeed"
+    },
+    "Fail": {
+      "Type": "Fail"
+    }
+  }
+}
